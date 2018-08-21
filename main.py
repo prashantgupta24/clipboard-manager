cfb9eff9e125404a0e268a1de42decee15799446
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-20 23:46:10 -0700

    buttons!

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-from tkinter import *
+from tkinter import Tk, Frame, Button, BOTH
 import time
 from threading import Thread
 
-def updateClipboard(T, root, clipboardContent):
+def onClick(root, cliptext):
+    #print("clicked!")
+    root.clipboard_clear()
+    root.clipboard_append(cliptext)
+
+def updateClipboard(root, clipboardContent, f):
     while True:
-        time.sleep(1)
+        # for child in f.children.values():
+        #     #print(dir(child))
+        #     child.destroy()
+        allButtons = [button for button in f.children.values()]
+        #print("number is ", len(allButtons))
+        time.sleep(0.1)
         cliptext = root.clipboard_get()
-        print("in loop")
-        if cliptext not in clipboardContent:
+        #print("in loop")
+        if cliptext not in clipboardContent and len(cliptext) < 200:
             clipboardContent.add(cliptext)
-            T.insert(END, cliptext + "\n\n")
-            T.pack()
+            if len(allButtons) == 2:
+                print("destroying!")
+                allButtons[0].destroy()
+            b = Button(f, text=cliptext, cursor="plus", wraplength=100, command=lambda: onClick(root, cliptext))
+            b.pack(fill=BOTH)
+            #b.grid(row=i, column=0)
             root.lift()
+        else:
+            #TODO trimming and saving full text in dict
+            pass
 
 def startMain():
     clipboardContent = set()
 
     root = Tk()
-    T = Text(root)
-    t = Thread(target=updateClipboard, args=(T, root, clipboardContent))
+    f = Frame(root, height=500, width=500)
+    f.pack_propagate(0) # don't shrink
+    f.pack()
+
+    t = Thread(target=updateClipboard, args=(root, clipboardContent, f))
     t.start()
     root.mainloop()
 
