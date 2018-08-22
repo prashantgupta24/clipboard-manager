0168f6d602ab1de2406cacf10532b2441cb9f9c2
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-22 00:32:35 -0700

    copy paste working

--- main.py
+++ main.py
@@ -3,37 +3,41 @@ import time
 from threading import Thread
 
 def onClick(root, cliptext):
-    #print("clicked!")
+    print("copied ", cliptext)
     root.clipboard_clear()
     root.clipboard_append(cliptext)
 
 def updateClipboard(root, clipboardContent, f):
+
+    truncateButtonLength = 30
+    maxClippings = 10
+
     while True:
-        # for child in f.children.values():
-        #     #print(dir(child))
-        #     child.destroy()
         allButtons = [button for button in f.children.values()]
-        #print("number is ", len(allButtons))
+        #print("buttons are ", len(allButtons))
         time.sleep(0.1)
-        cliptext = root.clipboard_get()
+        cliptextShort = cliptext = root.clipboard_get()
+
+        if len(cliptext) > truncateButtonLength:
+            cliptextShort = cliptext[:truncateButtonLength]+" ..."
         #print("in loop")
-        if cliptext not in clipboardContent and len(cliptext) < 200:
+        if cliptext not in clipboardContent:
             clipboardContent.add(cliptext)
-            if len(allButtons) == 2:
-                print("destroying!")
+
+            if len(allButtons) == maxClippings:
+                #print("destroying first button!")
                 allButtons[0].destroy()
-            b = Button(f, text=cliptext, cursor="plus", wraplength=100, command=lambda: onClick(root, cliptext))
+
+            b = Button(f, text=cliptextShort, cursor="plus", wraplength=100, command=lambda cliptext=cliptext: onClick(root, cliptext))
             b.pack(fill=BOTH)
-            #b.grid(row=i, column=0)
             root.lift()
-        else:
-            #TODO trimming and saving full text in dict
-            pass
 
 def startMain():
     clipboardContent = set()
 
     root = Tk()
+    root.title("My clipboard")
+
     f = Frame(root, height=500, width=500)
     f.pack_propagate(0) # don't shrink
     f.pack()
