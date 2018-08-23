2fc3184a47369544f580bc2f73b943a61908da65
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-23 00:09:05 -0700

    refactoring

--- clippy.py
+++ clippy.py
@@ -1,28 +1,37 @@
-from tkinter import Tk, Frame, Button, BOTH
+from tkinter import Tk, Frame, Button, BOTH, Widget
 import time
 from threading import Thread
 
 def onClick(root, cliptext):
-    print("copied ", cliptext)
+    #print("copied ", cliptext)
     root.clipboard_clear()
     root.clipboard_append(cliptext)
 
 def updateClipboard(root, clipboardContent, f):
 
-    truncateButtonLength = 30
+    truncateTextLength = 30
     maxClippings = 10
 
     while True:
         allButtons = [button for button in f.children.values()]
         #print("buttons are ", len(allButtons))
         time.sleep(0.1)
-        cliptextShort = cliptext = root.clipboard_get()
+        cliptext = root.clipboard_get()
 
-        if len(cliptext) > truncateButtonLength:
-            cliptextShort = cliptext[:truncateButtonLength]+" ..."
+        cliptext = "".join([c for c in cliptext if ord(c) <= 65535])
+
+        if not cliptext:
+            continue
+
+        if len(cliptext) > truncateTextLength:
+            cliptextShort = cliptext[:truncateTextLength]+" ..."
+        else:
+            cliptextShort = cliptext
         #print("in loop")
         if cliptext not in clipboardContent:
             clipboardContent.add(cliptext)
+            # print(cliptext)
+            # print([ord(x) for x in cliptext])
 
             if len(allButtons) == maxClippings:
                 #print("destroying first button!")
@@ -36,14 +45,14 @@ def startMain():
     clipboardContent = set()
 
     root = Tk()
-    root.title("My clipboard")
+    root.title("Clippy")
 
     f = Frame(root, height=500, width=500)
     f.pack_propagate(0) # don't shrink
     f.pack()
 
-    t = Thread(target=updateClipboard, args=(root, clipboardContent, f))
-    t.start()
+    Thread(target=updateClipboard, args=(root, clipboardContent, f)).start()
+    Widget.update(root)
     root.mainloop()
 
 if __name__ == '__main__':
