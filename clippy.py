d83616d8f86fd1edf7fc74e450e3edfedffe1a1d
Prashant Gupta <prashantgupta@us.ibm.com>
2018-08-26 13:02:50 -0700

    refactoring

--- clippy.py
+++ clippy.py
@@ -8,24 +8,28 @@ class Clippy(Frame):
         parent.resizable(False, False)
         self.pack_propagate(0)
         self.pack()
-        self.initMenu()
 
-        self.clipboardContent = set()
+        self.initDefaultValues()
         self.pollingFrequencyMs = 100
         self.truncateTextLength = 100
         self.maxClippingsOnApp = 10
         self.labelArray = []
-        self.labelIterVal = 0
         self.debug = False
 
+        self.initMenu()
         self.createLayout()
         self.updateClipboard()
+
+    def initDefaultValues(self):
+        self.clipboardContent = set()
+        self.clipboardContentMapping = {}
+        self.labelIterVal = 0
 
     def initMenu(self):
         menubar = Menu(self)
         optionsMenu = Menu(menubar, tearoff=0)
-        optionsMenu.add_command(label="Clear all (except last)", command=self.clearAll)
         optionsMenu.add_checkbutton(label="Always on top", command=self.toggleAlwaysOnTop)
+        optionsMenu.add_command(label="Clear all (except last)", command=self.clearAll)
         menubar.add_cascade(label="Options", menu=optionsMenu)
         self.parent.config(menu=menubar)
 
@@ -39,23 +43,24 @@ class Clippy(Frame):
     def updateClipboard(self):
         try:
             cliptext = self.clipboard_get()
-            #Handle empty clipboard content
-            if not cliptext:
-                return
+            if self.debug:
+                print("Called function, got ->", cliptext)
 
             cliptext, cliptextShort = self.cleanClipText(cliptext=cliptext)
             #Updating screen if new content found
-            if cliptextShort not in self.clipboardContent:
-                #print(cliptextShort)
-                self.clipboardContent.add(cliptextShort)
+            if cliptext not in self.clipboardContent and cliptextShort:
+
+                self.clipboardContent.add(cliptext)
+                self.clipboardContentMapping[cliptextShort] = cliptext
 
                 if self.labelIterVal == self.maxClippingsOnApp:
                     self.labelIterVal = 0
 
                 label = self.labelArray[self.labelIterVal]
                 labelText = label["text"]
-                if labelText in self.clipboardContent:
-                    self.clipboardContent.discard(labelText)
+                if labelText in self.clipboardContentMapping:
+                    self.clipboardContent.discard(self.clipboardContentMapping[labelText])
+                    self.clipboardContentMapping.pop(labelText)
                 label["text"] = cliptextShort
                 self.labelIterVal += 1
 
@@ -67,8 +72,13 @@ class Clippy(Frame):
 
         except TclError:
             pass #nothing on clipboard
+        except Exception as e:
+            print("ERROR!! -> ", e)
 
-        self.after(ms=self.pollingFrequencyMs, func=self.updateClipboard)
+        if self.debug:
+            self.after(ms=5000, func=self.updateClipboard)
+        else:
+            self.after(ms=self.pollingFrequencyMs, func=self.updateClipboard)
 
     def cleanClipText(self, cliptext):
         #Removing all characters > 65535 (that's the range for tkinter)
@@ -84,10 +94,12 @@ class Clippy(Frame):
 
     def onClick(self, labelNum):
         label = self.labelArray[labelNum]
+        if label["text"] == "":
+            return
         if self.debug:
-            print("copied ", label["text"])
+            print("copied ", self.clipboardContentMapping[label["text"]])
         self.clipboard_clear()
-        self.clipboard_append(label["text"])
+        self.clipboard_append(self.clipboardContentMapping[label["text"]])
         label["relief"] = SUNKEN
         self.after(ms=100, func=lambda label=label: self.animateClick(label))
 
@@ -95,9 +107,9 @@ class Clippy(Frame):
         label["relief"] = RAISED
 
     def clearAll(self):
-        for label in range(self.labelArray):
+        for label in self.labelArray:
             label["text"] = ""
-        self.clipboardContent = set()
+        self.initDefaultValues()
 
     def toggleAlwaysOnTop(self):
         if self.parent.attributes("-topmost") == 0:
